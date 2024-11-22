10b70a67a21f2781ded5698e63c2b2de6bab7dde
5hojib <yesiamshojib@gmail.com>
2024-11-22 23:30:59 +0600

    test

--- pyrogram/enums/chat_event_action.py
+++ pyrogram/enums/chat_event_action.py
@@ -113,6 +113,15 @@ class ChatEventAction(AutoName):
     MESSAGE_UNPINNED = auto()
     "a message has been unpinned (see ``unpinned_message``)"
 
+    CREATED_FORUM_TOPIC = auto()
+    "a new forum topic has been created (see `created_forum_topic`)"
+
+    EDITED_FORUM_TOPIC = auto()
+    "a forum topic has been edited (see `old_forum_topic` and `new_forum_topic`)"
+
+    DELETED_FORUM_TOPIC = auto()
+    "a forum topic has been deleted (see `deleted_forum_topic`)"
+
     MEMBER_SUBSCRIPTION_EXTENDED = auto()
     "A chat member extended their subscription to the chat (``old_chat_member`` and ``new_chat_member``)"
 
